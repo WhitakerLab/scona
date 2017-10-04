0d97f26785ee6f76a183cf9bddafd556f0fed347
islast <islastaden@gmail.com>
2017-10-04 14:55:05 +0100

    Change write_fixtures to contain overwrite_fixtures function as an option and delete overwrite_fixtures Add documentation

--- TESTS/write_fixtures.py
+++ TESTS/write_fixtures.py
@@ -1,17 +1,41 @@
-# To regression test our wrappers we need examples. This script generates some example files, and re-generates them for testing. We can then check that the files are identical
+#--------------------------- Write fixtures ---------------------------
+# To regression test our wrappers we need examples. This script 
+# generates files. We save these files once, and regression-tests.py
+# re-generates these files to tests them for identicality with the
+# presaved examples (fixtures). If they are found not to be identical 
+# it throws up an error. 
+#
+# (*someday* I would like to replace saving files with saving hashes)
+#
+# The point of this is to check that throughout the changes we make to 
+# BrainNetworksInPython the functionality of this script stays the same
+#
+# Currently the functionality of write_fixtures is to generate corrmat 
+# and network_analysis data via the functions 
+# corrmat_from_regionalmeasures and network_analysis_from_corrmat.
+-----------------------------------------------------------------------
                        
-def Recreate_correlation_matrix_fixture(folder):
+def recreate_correlation_matrix_fixture(folder):
+    ##### generate a correlation matrix in the given folder using #####
+    ##### the data in EXAMPLE_DATA                                ##### 
     import os
-    ##### generate the correlation matrix using the data in EXAMPLE_DATA #####  
     corrmat_path = os.getcwd()+folder+'/corrmat_file.txt'
     from corrmat_from_regionalmeasures import corrmat_from_regionalmeasures
-    corrmat_from_regionalmeasures("EXAMPLE_DATA/PARC_500aparc_thickness_behavmerge.csv", "EXAMPLE_DATA/500.names.txt", corrmat_path, names_308_style=True)
+    corrmat_from_regionalmeasures(
+        "EXAMPLE_DATA/PARC_500aparc_thickness_behavmerge.csv",
+        "EXAMPLE_DATA/500.names.txt", 
+        corrmat_path,
+        names_308_style=True)
      
-def Recreate_network_analysis_fixture(folder, corrmat_path):
-    ##### generate the network analysis using the data in EXAMPLE_DATA  and the correlation matrix given by corrmat_path #####  
+def recreate_network_analysis_fixture(folder, corrmat_path):
+    ##### generate network analysis in the given folder using the #####
+    ##### data in EXAMPLE_DATA and the correlation matrix given   #####
+    ##### by corrmat_path                                         #####  
     import os
+    # It is necessary to specify a random seed because 
+    # network_analysis_from_corrmat generates random graphs to 
+    # calculate global measures
     import random
-    # We need to specify a random seed to avoid complications with our randomly generated graphs
     random.seed(2984)
     from network_analysis_from_corrmat import network_analysis_from_corrmat
     network_analysis_from_corrmat(corrmat_path,
@@ -19,40 +43,43 @@ def Recreate_network_analysis_fixture(folder, corrmat_path):
                               "EXAMPLE_DATA/500.centroids.txt",
                               os.getcwd()+folder+'/network-analysis',
                               cost=10,
-                              n_rand=10,# this is not a reasonable n, but it saves us a lot of time
+                              n_rand=10, # this is not a reasonable 
+                              # value for n, we generate only 10 random
+                              # graphs to save time
                               names_308_style=True)
     
-def write_fixtures(folder, corrmat=True, net_analysis=True): ## corrmat and net_analysis arguments allow you to exclude them 
-    if (corrmat, net_analysis) == (None,None):
-        return
+def write_fixtures(folder='/tmp'): 
+    ## Run functions corrmat_from_regionalmeasures and               ##
+    ## network_analysis_from_corrmat to save corrmat in given folder ##
+    ##---------------------------------------------------------------##
+    #
+    # We want to prevent accidentally overwriting fixtures, so if the 
+    # function is saving in the test_fixtures folder it prompts for 
+    # approval.
+    if folder == '/test_fixtures':
+        if input('Are you sure you want to overwrite existing fixtures? y/n')!='y':
+            if input('Would you like to save new fixtures in a different folder?')!='y':
+                return
+            else:
+                folder = input('what would you like to name this folder')
+    # add WRAPPERS, EXAMPLE_DATA and SCRIPTS folders to the syspath
     import os
     import sys
-    if '/' not in folder:
-        folder = '/'+folder
     sys.path.append(os.path.abspath(os.path.join('WRAPPERS')))
     sys.path.append(os.path.abspath(os.path.join('EXAMPLE_DATA')))
     sys.path.append(os.path.abspath(os.path.join('SCRIPTS')))
-    print(sys.path)
+    # if the folder does not exist, create it
     if not os.path.isdir(os.getcwd()+folder):
         os.makedirs(os.getcwd()+folder)
-       
+    # generate and save the correlation matrix
     if corrmat == True:
         print("generating new correlation matrix") 
-        Recreate_correlation_matrix_fixture(folder)
-    
+        recreate_correlation_matrix_fixture(folder)
+    # generate and save the network analysis
     if net_analysis==True:
         print("generating new network analysis") 
         corrmat_path = 'TESTS/test_fixtures/corrmat_file.txt'
-        Recreate_network_analysis_fixture(folder, corrmat_path)
-        
-
-def overwrite_fixtures(corrmat=True, net_analysis=True): 
-# This function replaces the gold standard fixtures, use it wisely
-    if input('Are you sure you want to overwrite existing fixtures?  y/n  ') == 'y':
-        write_fixtures('test_fixtures',corrmat=corrmat,net_analysis=net_analysis)
-    elif input('Would you like to save new fixtures in a different folder?  ') == 'y':
-        folder = input('what would you like to name this folder')
-        write_fixtures(folder, corrmat=corrmat,net_analysis=net_analysis)
+        recreate_network_analysis_fixture(folder, corrmat_path)
 
 if __name__ == '__main__':
-    overwrite_fixtures()
+    write_fixtures()
