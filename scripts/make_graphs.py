36326ebaea4bdcd22617d9e04a452255cd546378
islast <islastaden@gmail.com>
2017-10-31 15:08:25 +0000

    tell travis to print graph degrees

--- scripts/make_graphs.py
+++ scripts/make_graphs.py
@@ -380,9 +380,9 @@ def calculate_nodal_measures(G, centroids, aparc_names, nodal_partition=None, na
     nodal_dict = {}
 
     #---- Degree ----------------------
-
-    print(type(G))
+    #import pdb; pdb.set_trace()
     print(type(G.degree()))
+    print(G.degree())
     deg = G.degree().values()
     nodal_dict['degree'] = list(deg)
 
