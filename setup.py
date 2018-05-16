4322dbe8417a2a4963243ce18fedf134eab787ee
islast <islastaden@gmail.com>
2018-05-16 09:39:50 +0100

    automatically find packages

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from distutils.core import setup
 
+from setuptools import find_packages
+PACKAGES = find_packages()
+
 if __name__ == '__main__':
     setup(
     name='BrainNetworksInPython',
     version='0.1dev',
-    packages=['scripts','wrappers'],
+    packages=PACKAGES,
     license='MIT license',
 )
