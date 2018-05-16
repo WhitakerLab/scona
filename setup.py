1c33e68ea8cc5b4665493d3f26570762cde00207
islast <islastaden@gmail.com>
2018-05-16 10:04:00 +0100

    setup data

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
-from distutils.core import setup
-
-from setuptools import find_packages
+from setuptools import setup, find_packages
 PACKAGES = find_packages()
+dir_datasets = 'BrainNetworksInPython/datasets'
+DATA = [(dir_datasets, glob(".txt"),
+        (dir_datasets, glob(".csv")]
 
 if __name__ == '__main__':
     setup(
     name='BrainNetworksInPython',
     version='0.1dev',
     packages=PACKAGES,
+    data_files=DATA
     license='MIT license',
 )
