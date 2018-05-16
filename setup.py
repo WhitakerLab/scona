f8274208aaccb0aad1637d3111c69e2f568aed7e
islast <islastaden@gmail.com>
2018-05-16 10:15:34 +0100

    specify package data files

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
 from setuptools import setup, find_packages
 PACKAGES = find_packages()
-dir_datasets = 'BrainNetworksInPython/datasets'
-DATA = [(dir_datasets, glob(".txt"),
-        (dir_datasets, glob(".csv")]
 
 if __name__ == '__main__':
     setup(
     name='BrainNetworksInPython',
     version='0.1dev',
     packages=PACKAGES,
-    data_files=DATA
+    package_data={'': ['.txt','.csv']}
     license='MIT license',
 )
