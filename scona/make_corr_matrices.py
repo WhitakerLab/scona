da4eb08319c1f99c95e16e88148ef600d48cfacd
Kirstie Whitaker <kw401@cam.ac.uk>
2018-09-12 15:27:45 +0100

    Make covars optional in create_residuals_df

--- scona/make_corr_matrices.py
+++ scona/make_corr_matrices.py
@@ -29,7 +29,7 @@ def get_non_numeric_cols(df):
     return non_numeric_cols
 
 
-def create_residuals_df(df, names, covars):
+def create_residuals_df(df, names, covars=[]):
     '''
     Calculate residuals of columns specified by names, correcting for the
     columns in covars_list.
@@ -37,14 +37,16 @@ def create_residuals_df(df, names, covars):
     Parameters
     ----------
     df : :class:`pandas.DataFrame`
-        a pandas data frame with subjects as rows and columns including
+        A pandas data frame with subjects as rows and columns including
         brain regions and covariates. Should be numeric for the columns in
-        names and covars_list
+        `names` and `covars`.
     names : list
-        a list of the brain regions you wish to correlate
-    covars: list
-        covars is a list of covariates (as df column headings)
-        to correct for before correlating the regions.
+        A list of the brain regions you wish to correlate.
+    covars: list, optional
+        A list of covariates to correct for before correlating
+        the regional measures. Each element should correspond to a
+        column heading in `df`.
+        Default is an empty list.
 
     Returns
     -------
@@ -54,8 +56,8 @@ def create_residuals_df(df, names, covars):
     Raises
     ------
     TypeError
-        if there are non numeric entries in the columns specified by names or
-        covars
+        if there are non numeric entries in the columns specified by `names` or
+        `covars`
     '''
     # Raise TypeError if any of the relevant columns are nonnumeric
     non_numeric_cols = get_non_numeric_cols(df[names+covars])
