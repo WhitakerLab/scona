a15b572b5dc525d897f478a6e6d70b53cf5c06f6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-09-12 12:30:53 +0000

    Deploy WhitakerLab/scona to github.com/WhitakerLab/scona.git:gh-pages

--- scona/datasets/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-Datasets module
-"""
-from . import NSPN_WhitakerVertes_PNAS2016
--- scona/wrappers/__init__.py
+++
@@ -1,2 +0,0 @@
-from scona.wrappers.corrmat_from_regionalmeasures import *
-from scona.wrappers.network_analysis_from_corrmat import *
