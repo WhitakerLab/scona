a5c4e39a419d0ac1cac1834129000a140939ee3b
Kirstie Whitaker <kw401@cam.ac.uk>
2018-09-12 16:17:04 +0100

    Update documentation for create_corrmat

--- scona/make_corr_matrices.py
+++ scona/make_corr_matrices.py
@@ -86,40 +86,44 @@ def create_residuals_df(df, names, covars=[]):
     return df_res
 
 
-def create_corrmat(df_residuals, names=None, method='pearson'):
+def create_corrmat(df_res, names=None, method='pearson'):
     '''
-    Correlate over the rows of df_residuals
+    Correlate over the rows of `df_res`
 
     Parameters
     ----------
     df_res : :class:`pandas.DataFrame`
-        df_res contains structural data about regions of the brain with
-        subjects as rows.
-    names : list
+        `df_res` contains structural data about regions of the brain with
+        subjects as rows after correction for any covariates of no interest.
+    names : list, optional
         The brain regions you wish to correlate over. These will become nodes
-        in your graph.
-    methods : string
-        the method of correlation passed to :func:`pandas.DataFram.corr`
+        in your graph. If `names` is None then all columns are included.
+        Default is `None`.
+    methods : string, optional
+        The method of correlation passed to :func:`pandas.DataFrame.corr`.
+        Default is pearsons correlation (`pearson`).
 
     Returns
     -------
     :class:`pandas.DataFrame`
-        A correlation matrix
+        A correlation matrix.
 
     Raises
     ------
     TypeError
-        if there are non numeric entries in the columns specified by names
+        If there are non numeric entries in the columns in `df_res` specified
+        by `names`.
     '''
     if names is None:
-        names = df_residuals.columns
+        names = df_res.columns
+
     # Raise TypeError if any of the relevant columns are nonnumeric
-    non_numeric_cols = get_non_numeric_cols(df_residuals)
+    non_numeric_cols = get_non_numeric_cols(df_res)
     if non_numeric_cols:
         raise TypeError('DataFrame columns {} are non numeric'
                         .format(', '.join(non_numeric_cols)))
 
-    return df_residuals.loc[:, names].astype(float).corr(method=method)
+    return df_res.loc[:, names].astype(float).corr(method=method)
 
 
 def corrmat_from_regionalmeasures(
