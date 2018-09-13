1142c14ec35685548c14ba9769e2933865082da9
islast <islastaden@gmail.com>
2018-09-13 13:37:27 +0100

    fill in some realease info

--- scona/__init__.py
+++ scona/__init__.py
@@ -1,37 +1,39 @@
 """
 scona
-========
+=====
 scona is a Python package for the analysis
 of structural covariance brain networks.
+
 Website (including documentation)::
-    http://whitakerlab.github.io
+    http://whitakerlab.github.io/scona
 Source::
     https://github.com/WhitakerLab/scona
 Bug reports::
     https://github.com/WhitakerLab/scona/issues
+
 Simple example
 --------------
-FILL
+    FILL
+
 Bugs
 ----
-Please report any bugs that you find `here <https://github.com/WhitakerLab/scona/issues>`_.
-Or, even better, fork the repository on GitHub and create a pull request (PR).
+Please report any bugs that you find in an issue on `our GitHub repository <https://github.com/WhitakerLab/scona/issues>`_.
+Or, even better, fork the repository and create a pull request.
+
 License
 -------
-FILL
+`scona  is licensed under the MIT License <https://github.com/WhitakerLab/scona/blob/master/LICENSE>`_.
 """
 
 # Release data
-__author__ = "FILL"
-__license__ = "FILL"
+__author__ = "Kirstie Whitaker and Isla Staden"
+__license__ = "MIT"
 
-__date__ = "FILL"
+__date__ = ""
 __version__ = 0.1
 
-__bibtex__ = """ FILL
+__bibtex__ = """ FILL 
 """
-
-# imports listed in chronological order
 
 from scona.make_corr_matrices import *
 from scona.make_graphs import *
@@ -39,4 +41,5 @@ from scona.graph_measures import *
 from scona.classes import *
 
 import scona.wrappers
+import scona.datasets
 from scona import *
