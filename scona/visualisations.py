42000e4576e4aa218b043d9968aed7faefda4bde
Kirstie Whitaker <kw401@cam.ac.uk>
2019-08-14 19:44:08 +0100

    change default for symm cmap

--- scona/visualisations.py
+++ scona/visualisations.py
@@ -479,14 +479,13 @@ def view_connectome_3d(
         G should have nodal locations in MNI space indexed by nodal
         attribute "centroids".
 
-    edge_threshold : str, number or None, optional (default="98%")
+    edge_threshold : str, number or None, optional (default="2%")
         If None, no thresholding.
         If it is a number only connections of amplitude greater
         than threshold will be shown.
         If it is a string it must finish with a percent sign,
-        e.g. "98%", and only connections of amplitude above the
-        given percentile will be shown (so "98%" shows the top "2%"
-        of connections).
+        e.g. "25.3%", and only connections of amplitude above the
+        given percentile will be shown.
 
     edge_cmap : str or matplotlib colormap, optional
         Colormap for displaying edges.
@@ -513,12 +512,6 @@ def view_connectome_3d(
 
     # get the adjacency matrix and nodes coordinates
     adj_matrix, node_coords = graph_to_nilearn_array(G)
-
-    # Figure out if you need to set some of the negative weights to zero
-    # This is useful if you're thresholding to only show the highest weighted
-    # edges and want to see the non symmetric colourmap.
-
-    ##### TODO!!
 
     # plot connectome
     ConnectomeView = plotting.view_connectome(adjacency_matrix=adj_matrix,
