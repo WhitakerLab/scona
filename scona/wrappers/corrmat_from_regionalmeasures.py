e27bbf4ebabb4e0a70dbf604c37026d08db4e19e
islast <islastaden@gmail.com>
2019-10-24 16:50:47 +0900

    implement patient grouping at correlation matrix level

--- scona/wrappers/corrmat_from_regionalmeasures.py
+++ scona/wrappers/corrmat_from_regionalmeasures.py
@@ -35,12 +35,11 @@ def setup_argparser():
         type=str,
         metavar='regional_measures_file',
         help=textwrap.dedent(
-            ('CSV file that contains regional values for each participant.\
+            ('A CSV file containing regional values for each participant.\
 \n') +
-            ('Column labels should be the region names or covariate \
+            ('Column labels should include the region names or covariate \
 variable\n') +
-            ('names. All participants in the file will be included in the\n') +
-            ('correlation matrix.')))
+            ('names. All participants in the file will be correlated over\n')))
 
     parser.add_argument(
         dest='names_file',
@@ -57,7 +56,22 @@ on each line.')))
         help=textwrap.dedent(
             ('File name of the output correlation matrix.\n') +
             ('If the output directory does not yet exist it will be \
-created.')))
+created.\n') +
+            ('If an argument is passed to `--group_by`, output names \
+will have the appropriate group coding appended to them'
+        ))
+
+    parser.add_argument(
+        'covariates',
+        type=list,
+        metavar='covariates',
+        help=textwrap.dedent(
+            ('List of variables that should be covaried for before \
+the creation\n') +
+            ('of the correlation matrix. Overrides `--covars_file` \
+argument below\n') +
+            ('Default: None')),
+        default=None)
 
     parser.add_argument(
         '--covars_file',
@@ -73,7 +87,7 @@ of the\n') +
         default=None)
 
     parser.add_argument(
-        '--method',
+        '--corr_method',
         type=str,
         metavar='method',
         help=textwrap.dedent(
@@ -81,42 +95,118 @@ of the\n') +
             ('options are "pearson", "spearman", "kendall"')),
         default='pearson')
 
+    parser.add_argument(
+        '--group_by',
+        type=str,
+        metavar='group_var',
+        help=textwrap.dedent(
+            ('This variable can be used to specify a column in \
+`regional_measures_file`\n') +
+            ('containing the group coding. A correlation matrix will be produced for each patient group.\n') +
+            ('  Default: None')),
+        default=None)
+    
     arguments = parser.parse_args()
 
     return arguments, parser
 
 
 def corrmat_from_regionalmeasures(regional_measures_file,
-                                  names_file,
-                                  output_name,
+                                  output_name=None,
+                                  covariates=None,
+                                  names_list=None,
+                                  names_file=None,
                                   covars_file=None,
-                                  method='pearson'):
+                                  method='pearson',
+                                  group_var=None):
     '''
     Read in regional measures, names and covariates files to compute
-    correlation matrix and write it to output_name.
+    and return a correlation matrix and write it to output_name.
 
-    Parameters:
-        * regional_measures_file : a csv containing data for some regional
-            measures with brain regions and covariates as columns and subjects
-            as rows. The first row of regional_measures should be column
-            headings.
-        * names_file : a text file containing names of brain regions. One name
-            per line of text. These names key columns in df to correlate over.
-        * covars_file : a text file containing a list of covariates to account
-            for. One covariate per line of text. These names key columns in df.
-        * output_name : file name to save output matrix to.
+    Parameters
+    ----------
+        regional_measures_file : str
+            A csv file of regional measures data containing any required
+            brain regions and covariates as columns. 
+            The first row of regional_measures should be column headings,
+            with subsequent rows corresponding to individual subjects.
+        names_file : str, optional
+            a text file containing names of brain regions. One name
+            per line of text. These names will specify the columns
+            in the regional_measures_file to correlate through. 
+            Failing to pass an argument either to both names_file and
+            names_list will cause an exception to be raised.
+        names_list : list, optional
+            a list of names of brain regions. These names will specify
+            the columns in the regional_measures_file to correlate
+            through. 
+            Failing to pass an argument to both names_file and
+            names_list will cause an exception to be raised.
+        covars_file : str, optional
+            a text file containing a list of covariates to account
+            for. One covariate per line of text. These covariates 
+            should index columns in regional_measure_file.
+        covariates : list, optional
+            a list of covariates to account for. These covariates
+            should index columns in regional_measures_file.    
+        output_name : str, optional
+            a file name to save output matrix to. 
+            If the output directory does not yet exist it will be
+            created.
+            If an argument is passed to group_var the group correlation
+            matrices will be written to a file with the corresponding
+            group coding appended.
+            If no name is passed the output matrix (or matrices) will
+            be returned but not saved.
+        group_var : str, optional
+            This variable can be used to specify a column in 
+            regional_measures_file containing the group coding. A
+            correlation matrix will be produced for each patient group.
+            
+
+    Returns
+    -------
+    :class:`pandas.DataFrame or dict
+        A correlation matrix, or a dictionary of group codings mapping to
+        correlation matrices. 
     '''
+    if names is None and names_file is None:
+        raise Exception(
+            "You must pass the names of brain regions you want to examine.\n"
+            + "Use either the `names_list` or the `names_file` argument.")
     # Read in the data
     df, names, covars_list, *a = read_in_data(
         regional_measures_file,
         names_file,
         covars_file=covars_file)
 
-    M = mcm.corrmat_from_regionalmeasures(
-        df, names, covars=covars_list, method=method)
+    if covariates is not None:
+        covars_list=covariates
 
-    # Save the matrix
-    mcm.save_mat(M, output_name)
+    if group_var is None:
+        # create correlation matrix
+        M = mcm.corrmat_from_regionalmeasures(
+            df, names, covars=covars_list, method=method)
+        if output_name is not None:
+            # Save the matrix
+            mcm.save_mat(M, output_name)
+        return M
+
+    else:
+        # split dataframe by group coding
+        df_by_group = split_groups(df, group_var)
+        matrix_by_group=dict()
+        # iterate over groups
+        for group_code, group_df in df_by_group:
+            # create correlation matrix
+            M = mcm.corrmat_from_regionalmeasures(
+            group_df, names, covars=covars_list, method=method)
+            if output_name is not None:
+                # Save the matrix
+                mcm.save_mat(M, output_name+str(group_code))
+            matrix_by_group[group_code] = M
+        return matrix_by_group
+            
 
 
 if __name__ == "__main__":
@@ -127,10 +217,12 @@ if __name__ == "__main__":
     # Now run the main function :)
     corrmat_from_regionalmeasures(
         arg.regional_measures_file,
-        arg.names_file,
-        arg.output_name,
+        names_file=arg.names_file,
+        output_name=arg.output_name,
+        covariates=arg.covariates,
         covars_file=arg.covars_file,
-        method=arg.method)
+        method=arg.method,
+        group_var=arg.group_var)
 
 # ============================================================================
 # Wooo! All done :)
